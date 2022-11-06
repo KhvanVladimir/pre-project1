942e0afbe22a9cd04b0e0c2f28cecb579b1204da
KhvanVladimir <khvan.vi0@gmail.rom>
2022-11-06 22:43:33 +0900

    1.1.4(1.1)

--- src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
@@ -4,30 +4,73 @@ import jm.task.core.jdbc.model.User;
 import jm.task.core.jdbc.util.Util;
 
 import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 
 public class UserDaoJDBCImpl implements UserDao {
-    public UserDaoJDBCImpl() {
 
-    }
+    private static final Connection connection = Util.getConnection();
 
     public void createUsersTable() {
-
+        try {
+            connection.createStatement().execute("CREATE TABLE Users (id INT NOT NULL AUTO_INCREMENT, name VARCHAR(45) NULL, lastName VARCHAR(45) NULL, age INT NULL, PRIMARY KEY (`id`))");
+        } catch (SQLException e) {
+            System.out.println("Ошибка при попытке создания таблицы");
+        }
     }
 
     public void dropUsersTable() {
+        try {
+            connection.createStatement().execute("drop table if exists users");
+        } catch (SQLException e) {
+            System.out.println("Ошибка при попытке удалить таблицу");
+        }
     }
 
     public void saveUser(String name, String lastName, byte age) {
+        try {
+            String query = String.format("INSERT INTO users values(null, '%s', '%s', '%d')", name, lastName, age);
+            connection.createStatement().execute(query);
+        } catch (SQLException e) {
+            System.out.println("Ошибка при попытке сохранить пользователя");
+        }
     }
 
     public void removeUserById(long id) {
+        try {
+            String query = String.format("DELETE FROM users WHERE id = %x", id);
+            connection.createStatement().execute(query);
+        } catch (SQLException e) {
+            System.out.println("Ошибка при попытке удалить пользователя");
+        }
     }
 
     public List<User> getAllUsers() {
-        return null;
+        List<User> l = new ArrayList<>();
+        try {
+            ResultSet result = connection.createStatement().executeQuery("select * from users");
+            while (result.next()) {
+                User u = new User();
+                u.setId(result.getLong("id"));
+                u.setName(result.getString("name"));
+                u.setLastName(result.getString("lastName"));
+                u.setAge(result.getByte("age"));
+                l.add(u);
+            }
+
+        } catch (SQLException e) {
+            System.err.println("Проблема ри попытке получить всех пользователей");
+        }
+        return l;
     }
 
     public void cleanUsersTable() {
+        try {
+            connection.createStatement().execute("DELETE FROM users");
+        } catch (SQLException e) {
+            System.out.println("Ошибка при попытке очистить таблицу пользователей");
+        }
     }
 }
--- src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
+++ src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
@@ -1,32 +1,36 @@
 package jm.task.core.jdbc.service;
 
+import jm.task.core.jdbc.dao.UserDao;
+import jm.task.core.jdbc.dao.UserDaoJDBCImpl;
 import jm.task.core.jdbc.model.User;
-import jm.task.core.jdbc.util.Util;
 
 import java.util.List;
 
 public class UserServiceImpl implements UserService {
-    public void createUsersTable() {;
-        Util.action("create");
+
+    UserDao a = new UserDaoJDBCImpl();
+
+    public void createUsersTable() {
+        a.createUsersTable();
     }
 
     public void dropUsersTable() {
-        Util.action("drop");
+        a.dropUsersTable();
     }
 
     public void saveUser(String name, String lastName, byte age) {
-        Util.saveUser(name, lastName, age);
+        a.saveUser(name, lastName, age);
     }
 
     public void removeUserById(long id) {
-        Util.removeUser(id);
+        a.removeUserById(id);
     }
 
     public List<User> getAllUsers() {
-        return Util.getUsers();
+        return a.getAllUsers();
     }
 
     public void cleanUsersTable() {
-        Util.cleanTable();
+        a.cleanUsersTable();
     }
 }
